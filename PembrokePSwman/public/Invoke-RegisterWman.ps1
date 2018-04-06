3c13f048fa8dd471effd8d9c6312cc68e8c8c064
jpsider <jpsider@gmail.com>
2018-04-05 22:40:16 -0400

    setting up register function

--- PembrokePSwman/public/Invoke-RegisterWman.ps1
+++ PembrokePSwman/public/Invoke-RegisterWman.ps1
@@ -4,21 +4,57 @@ function Invoke-RegisterWman {
 		This function will gather Status information from PembrokePS web/rest for a Queue_Manager
     .PARAMETER RestServer
         A Rest Server is required.
+    .PARAMETER Component_Id
+        A Component_Id is required.
+    .PARAMETER Wman_Type
+        A Wman_Type is Optional.
+    .PARAMETER LocalDir
+        A LocalDir is Optional.
 	.EXAMPLE
-        Invoke-RegisterWman -RestServer -localhost 
+        Invoke-RegisterWman -RestServer localhost -Component_Id 1 -Wman_Type Primary -LocalDir c:\PembrokePS
 	.NOTES
         This will return a hashtable of data from the PPS database.
     #>
     [CmdletBinding()]
     [OutputType([hashtable])]
     param(
-        [Parameter(Mandatory=$true)][string]$RestServer
+        [Parameter(Mandatory=$true)][string]$RestServer,
+        [Parameter(Mandatory=$true)][string]$Component_Id,
+        [string]$Wman_Type = "Primary",
+        [string]$LocalDir = "c:\PembrokePS"
     )
     if (Test-Connection -Count 1 $RestServer -Quiet) {
         try
         {
-            #Going to be creating a new record here, need to figure out the 'joins' to ensure the data is good.
-            Write-LogLevel -Message "lamp" -Logfile "$LOG_FILE" -RunLogLevel CONSOLEONLY -MsgLevel CONSOLEONLY
+
+    # Get System properties from Rest Server -> in pembrokepsRest
+    # Install required Modules -> in pembrokepsrest
+    # Create local directory structure -> In Utilities
+        # Move Files -> In utilities?
+    # Get Component specific properties. - > pembrokepsrest
+    # Write Properties file  -> In utilities
+            
+            
+            
+            
+            
+            
+            
+            
+            if(Test-Path -Path "$Destination\wman") {
+                Write-LogLevel -Message "Directory: $Destination, exists." -Logfile "$LOG_FILE" -RunLogLevel CONSOLEONLY -MsgLevel CONSOLEONLY
+            } else {
+                Write-LogLevel -Message "Creating \wman\data and \wman\logs Directories." -Logfile "$LOG_FILE" -RunLogLevel CONSOLEONLY -MsgLevel CONSOLEONLY
+                New-Item -Path "$Destination\wman\data" -ItemType Directory
+                New-Item -Path "$Destination\wman\logs" -ItemType Directory
+            }
+            Write-LogLevel -Message "Installing Dependent Modules." -Logfile "$LOG_FILE" -RunLogLevel CONSOLEONLY -MsgLevel CONSOLEONLY
+            Install-Module -Name PembrokePSrest,PembrokePSutilities,PowerLumber,RestPS -Force
+            Import-Module -Name PembrokePSrest,PembrokePSutilities,PowerLumber,RestPS -Force
+            Invoke-CreateRouteDirectorySet -InstallDirectory "$Destination\wman\rest"
+            Write-LogLevel -Message "Copying Properties file to $Destination\wman\data" -Logfile "$LOG_FILE" -RunLogLevel CONSOLEONLY -MsgLevel CONSOLEONLY
+            Copy-Item -Path "$Source\data\pembrokeps.properties" -Destination "$Destination\wman\data" -Confirm:$false       
+            Copy-Item -Path "$Source\bin\workflow_wrapper.ps1" -Destination "$Destination\wman\bin" -Confirm:$false
         }
         catch
         {
--- tests/public/Invoke-RegisterWman.tests.ps1
+++ tests/public/Invoke-RegisterWman.tests.ps1
@@ -1,34 +1,170 @@
 $script:ModuleName = 'PembrokePSwman'
 
-Describe "Invoke-RegisterWman function for $moduleName" {
+Describe "Invoke-DeployWman function for $moduleName" {
     function Write-LogLevel{}
-    It "Should not throw" {
-        Mock -CommandName 'Test-Connection' -MockWith {
+    function Invoke-CreateRouteDirectorySet {}
+    It "Should not Throw an exception if the source directory does exist." {
+        Mock -CommandName 'Test-Path' -MockWith {
             $true
         }
         Mock -CommandName 'Write-LogLevel' -MockWith {}
-        {Invoke-RegisterWman -RestServer dummyServer} | Should -not -Throw
-        Assert-MockCalled -CommandName 'Test-Connection' -Times 1 -Exactly
-        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 1 -Exactly
+        Mock -CommandName 'Install-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Import-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Invoke-CreateRouteDirectorySet' -MockWith {}
+        Mock -CommandName 'Copy-Item' -MockWith {}
+        {Invoke-DeployWman -Source 'c:\testdir'} | Should -Not -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Install-Module' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Import-Module' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Copy-Item' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-CreateRouteDirectorySet' -Times 1 -Exactly
+        
     }
-    It "Should Throw if the Rest Server cannot be reached.." {
-        Mock -CommandName 'Test-Connection' -MockWith {
+    It "Should Not throw, if the directories are created and files are moved.." {
+        Mock -CommandName 'Install-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Import-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Test-Path' -MockWith {
             $false
         }
+        Mock -CommandName 'New-Item' -MockWith {}
+        Mock -CommandName 'Invoke-CreateRouteDirectorySet' -MockWith {}
+        Mock -CommandName 'Copy-Item' -MockWith {}
         Mock -CommandName 'Write-LogLevel' -MockWith {}
-        {Invoke-RegisterWman -RestServer dummyServer} | Should -Throw
-        Assert-MockCalled -CommandName 'Test-Connection' -Times 2 -Exactly
-        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 1 -Exactly
+        {Invoke-DeployWman -Source 'c:\testdir'} | Should -Not -Throw
+        Assert-MockCalled -CommandName 'New-Item' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Copy-Item' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Test-Path' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-CreateRouteDirectorySet' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Install-Module' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Import-Module' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 6 -Exactly
+
     }
-    It "Should Throw if the Return is not valid." {
-        Mock -CommandName 'Test-Connection' -MockWith {
+    It "Should throw, if the directories are not created." {
+        Mock -CommandName 'Install-Module' -MockWith {
             $true
         }
-        Mock -CommandName 'Write-LogLevel' -MockWith {
-            Throw "Some failure to be udpated."
+        Mock -CommandName 'Import-Module' -MockWith {
+            $true
         }
-        {Invoke-RegisterWman -RestServer dummyServer} | Should -Throw
-        Assert-MockCalled -CommandName 'Test-Connection' -Times 3 -Exactly
-        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 2 -Exactly
+        Mock -CommandName 'Test-Path' -MockWith {
+            $false
+        }
+        Mock -CommandName 'Invoke-CreateRouteDirectorySet' -MockWith {}
+        Mock -CommandName 'New-Item' -MockWith {
+            Throw "Directory not created."
+        }
+        Mock -CommandName 'Copy-Item' -MockWith {}
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-DeployWman -Source 'c:\testdir'} | Should -Throw
+        Assert-MockCalled -CommandName 'New-Item' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Copy-Item' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Test-Path' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-CreateRouteDirectorySet' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Install-Module' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Import-Module' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 7 -Exactly
+    }
+    It "Should throw, Rest Directories are not created." {
+        Mock -CommandName 'Install-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Import-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Test-Path' -MockWith {
+            $false
+        }
+        Mock -CommandName 'Invoke-CreateRouteDirectorySet' -MockWith {
+            Throw "Unable to Create Rest Route Directories"
+        }
+        Mock -CommandName 'New-Item' -MockWith {}
+        Mock -CommandName 'Copy-Item' -MockWith {}
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-DeployWman -Source 'c:\testdir'} | Should -Throw
+        Assert-MockCalled -CommandName 'New-Item' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Copy-Item' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Test-Path' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-CreateRouteDirectorySet' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Install-Module' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Import-Module' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 9 -Exactly
+    }
+    It "Should throw, if the properties file is not copied." {
+        Mock -CommandName 'Install-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Import-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Test-Path' -MockWith {
+            $false
+        }
+        Mock -CommandName 'Invoke-CreateRouteDirectorySet' -MockWith {}
+        Mock -CommandName 'New-Item' -MockWith {}
+        Mock -CommandName 'Copy-Item' -MockWith {
+            Throw "Unable to move Items"
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-DeployWman -Source 'c:\testdir'} | Should -Throw
+        Assert-MockCalled -CommandName 'New-Item' -Times 7 -Exactly
+        Assert-MockCalled -CommandName 'Copy-Item' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Test-Path' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-CreateRouteDirectorySet' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Install-Module' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Import-Module' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 12 -Exactly
+    }
+    It "Should throw, if modules could not be installed." {
+        Mock -CommandName 'Install-Module' -MockWith {
+            Throw "Could not install Modules"
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        Assert-MockCalled -CommandName 'Install-Module' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 12 -Exactly
+    }
+    It "Should throw, if modules could not be Imported." {
+        Mock -CommandName 'Install-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Import-Module' -MockWith {
+            Throw "Could not Import Modules"
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        Assert-MockCalled -CommandName 'Install-Module' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Import-Module' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 12 -Exactly
+    }
+    function Split-Path {}
+    function Get-Module {}
+    It "Should not Throw an exception if the source directory does exist." {
+        Mock -CommandName 'Test-Path' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Install-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Import-Module' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        Mock -CommandName 'Invoke-CreateRouteDirectorySet' -MockWith {}
+        Mock -CommandName 'Copy-Item' -MockWith {}
+        {Invoke-DeployWman} | Should -Not -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 6 -Exactly
+        Assert-MockCalled -CommandName 'Install-Module' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Import-Module' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Copy-Item' -Times 7 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-CreateRouteDirectorySet' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 15 -Exactly
     }
 }
