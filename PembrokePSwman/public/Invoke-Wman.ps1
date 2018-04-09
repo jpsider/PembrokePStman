8646e8eebd2f609c2ead1fb16d322349294e0f48
jpsider <jsider@belaytech.com>
2018-04-09 17:28:15 -0400

    moved wman files to public. !deploy

---
+++ PembrokePSwman/public/Invoke-Wman.ps1
@@ -0,0 +1,100 @@
+function Invoke-Wman {
+    <#
+	.DESCRIPTION
+		This function will gather Status information from PembrokePS web/rest for a Workflow_Manager
+    .PARAMETER PropertyFilePath
+        A properties path is Required.
+	.EXAMPLE
+        Invoke-Wman -PropertyFilePath "c:\PembrokePS\Wman\pembrokeps.properties"
+	.NOTES
+        This will return a hashtable of data from the PPS database.
+    #>
+    [CmdletBinding()]
+    [OutputType([hashtable])]
+    [OutputType([Boolean])]
+    param(
+        [Parameter(Mandatory=$true)][string]$PropertyFilePath
+    )
+    if (Test-Path -Path $PropertyFilePath) {
+        # Gather Local Properties for the Workflow Manager
+        $PpsProperties = Get-LocalPropertySet -PropertyFilePath $PropertyFilePath
+        $RestServer = $PpsProperties.'system.RestServer'
+        $SystemRoot = $PpsProperties.'system.root'
+        $ID = $PpsProperties.'component.Id'
+        $RunLogLevel = $PpsProperties.'component.RunLogLevel'
+        $LOG_FILE = $PpsProperties.'component.logfile'
+        Write-LogLevel -Message "Gathering Local Properties from: $PropertyFilePath, SystemRoot: $SystemRoot." -Logfile $LOG_FILE -RunLogLevel CONSOLEONLY -MsgLevel CONSOLEONLY
+    } else {
+        Write-LogLevel -Message "Unable to Locate Local properties file: $PropertyFilePath." -Logfile $LOG_FILE -RunLogLevel CONSOLEONLY -MsgLevel CONSOLEONLY
+        Throw "Invoke-Wman: Unable to Locate Properties file."
+    }
+    try
+    {
+        $script:WmanRunning = "Running"
+        do {
+            # Test connection with the Database Server
+            Write-LogLevel -Message "Starting Invoke-Wman loop" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO
+            if (Test-Connection -Count 1 $RestServer -Quiet) {
+                # No Action needed if the RestServer can be reached.
+                $Host.UI.RawUI.WindowTitle = "Workflow_Manager WmanId:$ID"
+            } else {
+                $script:WmanRunning = "Shutdown"
+                Write-LogLevel -Message "Unable to reach RestServer: $RestServer." -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel ERROR
+                Throw "Invoke-Wman: Unable to reach Rest server: $RestServer."
+            }
+			Write-LogLevel -Message "Validated Connection to RestServer: $RestServer." -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+            # Get the Status and Workflow Manager Specific Information from the Database
+            $WmanStatusData = Get-WmanStatus -ComponentId $ID -RestServer $RestServer
+            $WorkflowManagerStatus = $WmanStatusData.STATUS_ID
+            $LOG_FILE = $WmanStatusData.LOG_FILE
+            $ManagerWait = $WmanStatusData.WAIT
+            $MAX_CONCURRENT_TASKS = $WmanStatusData.MAX_CONCURRENT_TASKS
+            $WORKFLOW_MANAGER_TYPE_ID = $WmanStatusData.WORKFLOW_MANAGER_TYPE_ID
+			Write-LogLevel -Message "Get-WmanStatus is complete" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel DEBUG 
+            $TableNameData = Get-WmanTableName -RestServer $RestServer -Type_ID $WORKFLOW_MANAGER_TYPE_ID
+            $TableName = ($TableNameData).TABLENAME
+			Write-LogLevel -Message "Get WmanTablename is complete, TableName: $TableName" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO
+            # Based on the Status Perform Specific actions
+            Write-LogLevel -Message "WorkflowManager ID:                     $ID" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+            Write-LogLevel -Message "WorkflowManager Status:                 $WorkflowManagerStatus" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+            Write-LogLevel -Message "WorkflowManager TableName :             $TableName" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+            Write-LogLevel -Message "WorkflowManager Wait:                   $ManagerWait" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+            Write-LogLevel -Message "WorkflowManager MAX_CONCURRENT_TASKS:   $MAX_CONCURRENT_TASKS" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+            Write-LogLevel -Message "WorkflowManager LogFile:                $LOG_FILE" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+            if ($WorkflowManagerStatus -eq 1) {
+                # Down - Not doing Anything
+                Write-LogLevel -Message "Get-WmanStatus is Down, Not taking Action." -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel ERROR 
+            } elseif ($WorkflowManagerStatus -eq 2) {
+                # Up - Perform normal Tasks
+                Write-LogLevel -Message "Get-WmanStatus is UP, performing Normal Operations" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+                Write-LogLevel -Message "Reviewing Assigned Tasks" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+                Invoke-ReviewAssignedTaskSet -RestServer $RestServer -TableName $TableName -MAX_CONCURRENT_TASKS $MAX_CONCURRENT_TASKS -ID $ID
+                Write-LogLevel -Message "Normal Operations Completed." -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel DEBUG 
+            } elseif ($WorkflowManagerStatus -eq 3) {
+                # Starting Up - Perform startup Tasks
+                Write-LogLevel -Message "Performting Startup Tasks" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+                Invoke-WmanStartupTaskSet -TableName $TableName -RestServer $RestServer -ID $ID
+                Write-LogLevel -Message "Startup Tasks Completed." -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel DEBUG
+            } elseif ($WorkflowManagerStatus -eq 4) {
+                # Shutting Down - Perform Shutdown Tasks
+                Write-LogLevel -Message "Performing Shutdown tasks." -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+                Invoke-WmanShutdownTaskSet -TableName $TableName -RestServer $RestServer -ID $ID
+                $script:WmanRunning = "Shutdown"
+                Write-LogLevel -Message "Shutdown tasks completed." -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+            }
+            Write-LogLevel -Message "Manager running String: $script:WmanRunning" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel DEBUG 
+            if($script:WmanRunning -ne "Shutdown"){
+                Write-LogLevel -Message "Waiting $ManagerWait Seconds" -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+                Invoke-Wait -Seconds $ManagerWait
+            }
+        } while ($script:WmanRunning -ne "Shutdown")
+        Write-LogLevel -Message "Exiting WorkflowManager Function." -Logfile $LOG_FILE -RunLogLevel $RunLogLevel -MsgLevel INFO 
+    }
+    catch
+    {
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName		
+        Throw "Invoke-Wman: $ErrorMessage $FailedItem"
+    }
+    
+}
---
+++ tests/public/Invoke-Wman.tests.ps1
@@ -0,0 +1,243 @@
+$script:ModuleName = 'PembrokePSwman'
+
+Describe "Invoke-Wman function for $moduleName" {
+    function Get-LocalPropertySet{}
+    function Get-WmanStatus{}
+    function Invoke-Wait{}
+    function Get-WmanTableName{}
+    function Write-LogLevel{}
+    It "Should Throw if the path fails" {
+        Mock -CommandName 'Test-Path' -MockWith {
+            $false
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-Wman -PropertyFilePath 'c:\pps\Wman\pembrokeps.properties'} | Should -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 1 -Exactly
+    }
+    It "Should not Throw during Shutdown Tasks" {
+        Mock -CommandName 'Test-Path' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-LocalPropertySet' -MockWith { 
+            $PpsProperties = @{
+                system = @{
+                    RestServer = 'localhost'
+                }
+                component = @{
+                    id = '1'
+                }
+            }
+            return $PpsProperties
+        }
+        function Test-Connection {}
+        Mock -CommandName 'Test-Connection' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-WmanStatus' -MockWith {
+            $RawReturn = @(
+                @{
+                    ID            = '1'
+                    STATUS_ID     = '4'
+                    WAIT       = '300'
+                }               
+            )
+            $ReturnJson = $RawReturn | ConvertTo-Json
+            $WmanStatusData = $ReturnJson | convertfrom-json
+            return $WmanStatusData
+        }
+        Mock -CommandName 'Get-WmanTableName' -MockWith {
+            return "tasks"
+        }
+        function Invoke-WmanShutdownTaskSet{}
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        Mock -CommandName 'Invoke-WmanShutdownTaskSet' -MockWith {}
+        {Invoke-Wman -PropertyFilePath 'c:\pps\Wman\pembrokeps.properties'} | Should -not -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Test-Connection' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Get-LocalPropertySet' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Get-WmanStatus' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-WmanShutdownTaskSet' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 16 -Exactly
+    }
+    It "Should not Throw during StartUp Tasks" {
+        Mock -CommandName 'Test-Path' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-LocalPropertySet' -MockWith { 
+            $PpsProperties = @{
+                system = @{
+                    RestServer = 'localhost'
+                }
+                component = @{
+                    id = '1'
+                }
+            }
+            return $PpsProperties
+        }
+        function Test-Connection {}
+        Mock -CommandName 'Test-Connection' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-WmanStatus' -MockWith {
+            $RawReturn = @(
+                @{
+                    ID            = '1'
+                    STATUS_ID     = '3'
+                    WAIT       = '300'
+                }               
+            )
+            $ReturnJson = $RawReturn | ConvertTo-Json
+            $WmanStatusData = $ReturnJson | convertfrom-json
+            return $WmanStatusData
+        }
+        Mock -CommandName 'Get-WmanTableName' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Invoke-Wait' -MockWith {
+            $script:WmanRunning = "shutdown"
+            return $script:WmanRunning
+        }
+        function Invoke-WmanStartupTaskSet{}
+        Mock -CommandName 'Invoke-WmanStartupTaskSet' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-Wman -PropertyFilePath 'c:\pps\Wman\pembrokeps.properties'} | Should -not -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Test-Connection' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Get-LocalPropertySet' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Get-WmanStatus' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-Wait' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-WmanStartupTaskSet' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 32 -Exactly
+    }
+    It "Should not Throw during Normal Operation" {
+        Mock -CommandName 'Test-Path' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-LocalPropertySet' -MockWith { 
+            $PpsProperties = @{
+                system = @{
+                    RestServer = 'localhost'
+                }
+                component = @{
+                    id = '1'
+                }
+            }
+            return $PpsProperties
+        }
+        function Test-Connection {}
+        Mock -CommandName 'Test-Connection' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-WmanStatus' -MockWith {
+            $RawReturn = @(
+                @{
+                    ID            = '1'
+                    STATUS_ID     = '2'
+                    WAIT       = $null
+                }               
+            )
+            $ReturnJson = $RawReturn | ConvertTo-Json
+            $WmanStatusData = $ReturnJson | convertfrom-json
+            return $WmanStatusData
+        }
+        Mock -CommandName 'Get-WmanTableName' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Invoke-Wait' -MockWith {
+            $script:WmanRunning = "shutdown"
+            return $script:WmanRunning
+        }
+        function Invoke-ReviewAssignedTaskSet{}
+        Mock -CommandName 'Invoke-ReviewAssignedTaskSet' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-Wman -PropertyFilePath 'c:\pps\Wman\pembrokeps.properties'} | Should -not -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Test-Connection' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Get-LocalPropertySet' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Get-WmanStatus' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-Wait' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-ReviewAssignedTaskSet' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 49 -Exactly
+    }
+    It "Should not Throw if the Status starts as Down" {
+        Mock -CommandName 'Test-Path' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-LocalPropertySet' -MockWith { 
+            $PpsProperties = @{
+                system = @{
+                    RestServer = 'localhost'
+                }
+                component = @{
+                    id = '1'
+                }
+            }
+            return $PpsProperties
+        }
+        function Test-Connection {}
+        Mock -CommandName 'Test-Connection' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-WmanStatus' -MockWith {
+            $RawReturn = @(
+                @{
+                    ID            = '1'
+                    STATUS_ID     = '1'
+                    WAIT       = $null
+                }               
+            )
+            $ReturnJson = $RawReturn | ConvertTo-Json
+            $WmanStatusData = $ReturnJson | convertfrom-json
+            return $WmanStatusData
+        }
+        Mock -CommandName 'Get-WmanTableName' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Invoke-Wait' -MockWith {
+            $script:WmanRunning = "shutdown"
+            return $script:WmanRunning
+        }
+        function Invoke-ReviewAssignedTaskSet{}
+        Mock -CommandName 'Invoke-ReviewAssignedTaskSet' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-Wman -PropertyFilePath 'c:\pps\Wman\pembrokeps.properties'} | Should -not -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Test-Connection' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Get-LocalPropertySet' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Get-WmanStatus' -Times 4 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-Wait' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Invoke-ReviewAssignedTaskSet' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 64 -Exactly
+    }
+    It "Should Throw if the Rest Server cannot be reached.." {
+        Mock -CommandName 'Test-Path' -MockWith {
+            $true
+        }
+        Mock -CommandName 'Get-LocalPropertySet' -MockWith {
+            return $PpsProperties
+        }
+        Mock -CommandName 'Test-Connection' -MockWith {
+            $false
+        }
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-Wman -PropertyFilePath "c:\pps\Wman\pembrokeps.properties"} | Should -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 6 -Exactly
+        Assert-MockCalled -CommandName 'Test-Connection' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Get-LocalPropertySet' -Times 5 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 67 -Exactly
+    }
+    It "Should Throw if the file does not exist." {
+        Mock -CommandName 'Test-Path' -MockWith {}
+        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        {Invoke-Wman -PropertyFilePath "c:\pps\Wman\pembrokeps.properties"} | Should -Throw
+        Assert-MockCalled -CommandName 'Test-Path' -Times 7 -Exactly
+        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 68 -Exactly
+    }
+}
